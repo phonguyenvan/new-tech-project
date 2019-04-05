b15fff2d39012ab1b24bde61cf0109f7932ca95e
Van Pho Nguyen <vanpho02@gmail.com>
2019-04-05 11:39:18 +0700

    all test passed

--- server/src/play.ts
+++ server/src/play.ts
@@ -1,34 +1,10 @@
-import { User, Todo } from './refs';
-import { Sequelize, Includeable } from 'sequelize';
-// insert users
+import * as td from 'testdouble';
 
-async function insertUser() {
-    await User.create({
-        email: 'vanpho01@gmail.com',
-        name: 'Van Pho Nguyen',
-        language: 'vn'
-    });
-}
-
-async function queryUserById(id: string | number) {
-    const user = await User.findOne({ where: { user_id: id }, include: [{ model: Todo, where: { user_id: Sequelize.col('todos.user_id') } }] });
-    console.log(user.toJSON());
-}
-
-async function queryTodoById(id: string | number) {
-    const todo = await Todo.findOne({ where: { todo_id: id }, include: [{ model: User, where: { user_id: Sequelize.col('todos.user_id') } }] });
-    console.log(todo.toJSON());
-}
-
-async function queryAllTodos() {
-    const todos = await Todo.findAll({});
-    const data = todos.map(todo => todo.toJSON());
-    console.log(data);
-}
-
-async function start() {
-    await queryAllTodos();
-    process.exit(0);
+function start() {
+    const queryUserById = td.replace('./refs').queryUserById;
+    td.when(queryUserById(1)).thenReturn({ name: 'Pho' });
+    const result = queryUserById(1);
+    console.log(result);
 }
 
 start();
--- server/src/test/query-todo-by-id.test.ts
+++ server/src/test/query-todo-by-id.test.ts
@@ -1,40 +1,40 @@
-import test from 'ava';
+import test, { todo } from 'ava';
 import { queryTodoById, TodoModel, queryUserById, UserModel, queryAllTodos, queryAllUsers, knex } from '../refs';
 
-async function removeData() {
-    await knex('todos').del();
-    await knex('users').del();
-}
+let user1, user2, user3, todo1;
 
 async function importData() {
+    await knex('todos').del();
+    await knex('users').del();
     await knex('users').insert([
         { name: 'Pho', email: 'phonguyen@gmail.com', language: 'vn' },
         { name: 'Ngan', email: 'ngannguyen@gmail.com', language: 'en' },
         { name: 'Tran', email: 'trannguyen@gmail.com', language: 'fn' },
     ]);
+    [user1, user2, user3] = await knex.select('user_id', 'name').from('users');
     await knex('todos').insert([
-        { description: 'Pho creates users.', user_id: 1 },
-        { description: 'Pho creates more users.', user_id: 1 },
-        { description: 'Pho creates more clients.', user_id: 1 },
-        { description: 'Ngan creates Todos.', user_id: 2 },
-        { description: 'Ngan creates big mistake.', user_id: 2 },
-        { description: 'Tran creates Migrations.', user_id: 3 },
+        { description: 'Pho creates users.', user_id: user1.user_id },
+        { description: 'Pho creates more users.', user_id: user1.user_id },
+        { description: 'Pho creates more clients.', user_id: user1.user_id },
+        { description: 'Ngan creates Todos.', user_id: user2.user_id },
+        { description: 'Ngan creates big mistake.', user_id: user2.user_id },
+        { description: 'Tran creates Migrations.', user_id: user3.user_id },
     ]);
+    [todo1] = await knex.select('todo_id', 'description').from('todos');
 }
 
-test.serial.beforeEach('Import data for test', async () => {
-    await removeData();
+test.before('Import data for test', async () => {
     await importData();
 });
 
 test('get todo by id', async (t) => {
-    const todo = await queryTodoById(1) as TodoModel;
-    t.is(todo.todo_id, 1);
+    const todo = await queryTodoById(todo1.todo_id) as TodoModel;
+    t.is(todo.todo_id, todo1.todo_id);
 });
 
 test('get user by id', async (t) => {
-    const user = await queryUserById(2) as UserModel;
-    t.is(user.user_id, 2);
+    const user = await queryUserById(user2.user_id) as UserModel;
+    t.is(user.user_id, user2.user_id);
 });
 
 test('get all users', async (t) => {
